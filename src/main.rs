219cdd8ea109ab63858b88e73fca4db7616a931e
Lasse Hels <lasse.hels@maersk.com>
2024-10-12 13:55:18 +0200

    add settings

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use k8s_openapi::serde::Deserialize;
+
 mod reconciler;
+mod settings;
 mod tainter;
 
 #[actix_web::main]
 async fn main() {
+    // TODO read settings file and initialize.
+    // let settings = Config::builder().add_source(config::File::with_name("example_config")).build().unwrap();
+    //
+    // println!("{:?}", settings);
+
     tracing_subscriber::fmt()
         .json()
         // TODO ability to configure log level.
@@ -10,6 +18,6 @@ async fn main() {
         .with_current_span(false)
         .init();
 
-    let tainter = tainter::Tainter::new("localhost".to_string(), 8000);
-    tainter.start().await.unwrap();
+    // let tainter = tainter::Tainter::new("localhost".to_string(), 8000);
+    // tainter.start().await.unwrap();
 }
